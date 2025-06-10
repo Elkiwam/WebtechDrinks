744ae7454eb83d2892e2c1866a6e072de60c9ff3
Timon Rimann <timon.rimann@zhdk.ch>
2025-06-10 17:15:43 +0200

    feat: add description collapsible, add: ingredient counter description

--- card2/js/main.js
+++ card2/js/main.js
@@ -1,11 +1,17 @@
 console.log("...fetching a random cocktail 🍹");
 
-// prepare dom elements
+// prepare needed dom elements
 const titleContainer = document.querySelector("[data-js='title']");
 const imgContainer = document.querySelector("[data-js='photo']");
+// glass type container
+const glassType = document.querySelector("[data-js='glassType']");
+//ingredient counter
 const ingredientList = document.querySelector("[data-js='ingredients']");
 const ingredients = new Array();
-const glassType = document.querySelector("[data-js='glassType']");
+const ingrCounter = document.querySelector("[data-js='ingrCounter']");
+//  preparation instructions
+const preparationContainer = document.querySelector("[data-js='preparation']");
+
 
 fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
   .then((response) => response.json())
@@ -34,10 +40,11 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
       const measure = drink[`strMeasure${i}`];
 
       if (ingredient) {
-        const formattedMeasure = measure ? measure : '';
-        ingredients[i - 1] = `${formattedMeasure}${ingredient}`.trim();
+      const formattedMeasure = measure ? `${measure} ` : '';
+      ingredients[i - 1] = `${formattedMeasure}${ingredient}`.trim();
       }
     }
+
 
     // check in console
     console.log("All Ingredients:", ingredients);
@@ -54,6 +61,8 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
 // check glass type in console
 console.log("Glass Type:", drink.strGlass);
 
+
+
 // Assign different images to each glass type
 //try to sanitize /rewrite 
 const glassImages = {
@@ -61,7 +70,7 @@ const glassImages = {
   "Cocktail glass": "./glasses/martini-glass.svg",
   "Old-fashioned glass": "./glasses/old-fashioned.svg",
   "Collins Glass": "./glasses/collins-glass.svg",
-  //"Collins glass": "./glasses/collins-glass.svg",
+  "Collins glass": "./glasses/collins-glass.svg",
   "Champagne flute": "./glasses/champagne-flute.svg",
   "Whiskey glass": "./glasses/whiskey-glass.svg",
   "Martini glass": "./glasses/martini-glass.svg",
@@ -85,9 +94,51 @@ glassImg.src = glassImageSrc;
 glassImg.alt = drink.strGlass;
 glassType.appendChild(glassImg);
 
+// Label the drink based on the number of ingredients
+const ingredientCount = ingredients.filter(ingredient => ingredient).length;
+console.log("Ingredients Count:", ingredientCount);
+
+if (ingredientCount <= 3) {
+  console.log("This drink difficulty is labeled as: Quick and Easy");
+  ingrCounter.innerHTML += `Quick and Easy  (${ingredientCount} ingredients)`;
+} else if (ingredientCount > 6) {
+  console.log("This drink difficulty is labeled as: Impressive");
+  ingrCounter.innerHTML += `Impressive  (${ingredientCount} ingredients)`;
+} else {
+  console.log("This drink is labeled as: Worth it!");
+  ingrCounter.innerHTML += ` Worth it! (${ingredientCount} ingredients)`;
+}
+
+  // Check if the drink has preparation instructions
+  if (drink.strInstructions) {
+    // Display the preparation instructions in the container
+    preparationContainer.innerHTML = `<p>${drink.strInstructions}</p>`;
+  } else {
+    // If no instructions are available, display a default message
+    preparationContainer.innerHTML = `<p>Preparation instructions are not available for this drink.</p>`;
+  }
+
+  console.log("prep:", drink.strInstructions);
 
   })
   .catch((error) => {
     // Handle any errors that occur during the fetch request
     console.error("Error:", error);
   });
+
+
+  // event listener for description collapsible
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
